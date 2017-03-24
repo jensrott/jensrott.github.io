2ad5878d41da18a92a392dd0f1dbcf1510198390
jensrott <jensrott@student.arteveldehs.be>
2017-03-24 13:28:25 +0100

    test pagina

--- js/greetings.js
+++ js/greetings.js
@@ -1,15 +1,1 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
 
-if (hourNow > 18) {
-	greeting = 'Goede Avond!';
-} else if (hourNow > 12) {
-	greeting = 'Goede Middag!';
-} else if (hourNow > 0) {
-	greeting = 'Goede Morgen!';
-} else {
-	greeting = 'Welkom!';
-}
-
-document.write(greeting);
